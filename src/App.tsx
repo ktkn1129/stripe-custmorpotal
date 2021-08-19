29b6be4be5f6e5e49adb45afcb7cdb302a5b8c86
ktkn1129 <kitagawa.kana@classmethod.jp>
2021-08-19 23:54:46 +0900

    add buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
+import liff from '@line/liff';
 import './App.css';
+import * as dotenv from 'dotenv';
+
 
 function App() {
+  /* 追加: メッセージの送信 */
+  const sendMessage = () => {
+    liff.init({liffId: process.env.REACT_APP_LIFF_ID as string}) // LIFF IDを設定する
+    .then(() => {
+      if(!liff.isLoggedIn()) {
+        liff.login({}) // ログインしてなければログインを行う
+      } else if (liff.isInClient()){ // LIFFアプリをLIFFブラウザで起動すれば
+        liff.sendMessages([{
+          'type': 'text',
+          'text': "You've successfully sent a message! Yeah!"
+        }]).then(function(){
+          window.alert('Message sent');
+        }).catch(function(error){
+          window.alert('Error sending message: ' + error);
+        });
+      }
+    })
+  }
+
+  /* 追加: UserProfileをAlertで表示 */
+  const getUserInfo = () => {
+    liff.init({liffId: process.env.REACT_APP_LIFF_ID as string})
+    .then(() => {
+      if(!liff.isLoggedIn()) {
+        liff.login({}) // ログインしてなければログインを行う
+      } else if (liff.isInClient()){ // LIFFアプリをLIFFブラウザで起動すれば
+        liff.getProfile() // ユーザー情報を取得する
+        .then(profile =>{
+          const userId: string = profile.userId
+          const displayName: string = profile.displayName
+          alert(`Name: ${displayName}, userId: ${userId}`)
+        }).catch(function(error){
+          window.alert('Error sending message: ' + error);
+        });
+      }
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className="button" onClick={sendMessage}>send message</button>
+        <button className="button" onClick={getUserInfo}>get User Info</button>
       </header>
     </div>
   );
